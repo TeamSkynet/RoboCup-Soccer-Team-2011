95bc72c3dd9e9a959a58af05ccb4d77be101b005
Joel Tanzi <jtanziredux@gmail.com>
2011-09-27 16:41:24 -0500

    Updated complist with file compile list, and README with instructions.

---
+++ SenseMemory.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class SenseMemory {
+	public SenseMemory(ArrayList<SenseInfo> newSenseArray, int t) {
+		this.SenseArray = newSenseArray;
+		this.time = t;
+	}
+	
+	public void addInfo(SenseInfo newInfo) {
+		SenseArray.add(newInfo);
+	}
+	
+	
+	public int getTime() {
+		return time;
+	}
+	
+	public void setTime(int t) {
+		this.time = t;
+	}
+	
+	public void setTime(String[] seeOrSense) {
+		this.time = Integer.parseInt(seeOrSense[1]);
+	}
+	
+	public int getSize() {
+		return SenseArray.size();
+	}
+	
+	public SenseInfo getObj(int index) {
+		return SenseArray.get(index);
+	}
+
+	public ArrayList<SenseInfo> SenseArray;
+	private int time;
+	
+	
+}
