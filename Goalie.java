c6693752a1fb9d8340904576e5ba3350db14b735
Grant Hays <granthays@gmail.com>
2011-10-31 14:19:42 -0500

    Added methods for Goalie to tell if ball is in goal. Needs some work. Also GoalieTest sort of shows how it works if a player kicks against him.

--- Goalie.java
+++ Goalie.java
@@ -43,5 +43,94 @@ public class Goalie extends Player {
 	public void catchball(int dir) throws UnknownHostException{
 		rc.catchball(dir);
 	}
+	
+	public void followBall() {
+		
+		
+		try {
+			if(!getMem().isObjVisible("ball") && !ballTurn) {
+				getRoboClient().turn(45);
+				ballTurn = true;
+			}
+			else if(!getMem().isObjVisible("ball") && ballTurn) {
+				getRoboClient().turn(-45);
+				ballTurn = false;
+			}
+			
+			if(getMem().isObjVisible("ball")) {
+				ObjBall ball = getMem().getBall();
+				
+				if((ball.getDirection() > 5.0) || (ball.getDirection() < -5.0)) {
+					getRoboClient().turn(ball.getDirection() * (1 + (5 * getMem().getAmountOfSpeed())));
+				}
+					
+				if(ballInGoalzone(ball)) {
+					//
+					// I think you'd probably go to defendGoal() after this. I was thinking
+					// you should probably pass in the ball to defendGoal, too, so you can use
+					// something like getNextBallPoint(ObjBall ball) in MathHelp
+					//
+					System.out.println("I see you BALL");
+				}
+			}
+			
+		} catch (UnknownHostException e) {
+			System.out.println("Error in Goalie.followBall()");
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean ballInGoalzone(ObjBall ball) {
+		try {
+			
+			ObjFlag PFlag = getMem().getClosestPenaltyFlag();
+			
+			if(PFlag == null) {
+				getRoboClient().turn(-1 * getMem().getDirectionOfSpeed());
+				System.out.println("No Penalty flag in sight");
+				return false;
+			}
+			else {
+				
+				Pos toFlag = mh.getPos(PFlag.getDistance(), PFlag.getDirection());
+				Pos ballPos = mh.getPos(ball.getDistance(), ball.getDirection());
+				
+				if(toFlag.x >= ballPos.x)
+					return true;
+				else
+					return false;
+				
+				
+			}
+			
+		} catch (UnknownHostException e) {
+			System.out.println("Error in Goalie.ballInGoalZone()");
+			e.printStackTrace();
+		}
+		
+		
+		
+		return false;
+	}
+	
+	public void defendGoal(ObjBall ball) {
+		
+		
+		
+		//If ball in goal zone
+		
+		//Determine ball position in relation to goalie
+		
+		
+		//Move if needed
+		
+		//If ball is in catchable area
+		
+		//Catch ball
+		
+	}
+	
+	public boolean ballTurn = false;
+	public MathHelp mh = new MathHelp();
 
 } //end class
