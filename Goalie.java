9e3058b169590effbff37887d5fcd14e1ce7d537
Joel Tanzi <jtanziredux@gmail.com>
2011-11-05 10:57:01 -0500

    Fixed merge errors in Goalie

--- Goalie.java
+++ Goalie.java
@@ -42,90 +42,9 @@ public class Goalie extends Player {
 	 */
 	public void catchball(double d) throws UnknownHostException{
 		rc.catchball(d);
-<<<<<<< HEAD
 	}
 
-	/**
-	 * Gets absolute position of Goalie on field
-	 * @post Pos with x and y coordinates of Goalie on field
-	 * @return Pos coordinates of position
-	 */
-	public Pos getPosition() {
 
-		ObjFlag flag = getMem().getClosestPenaltyFlag();
-		ObjGoal goal = getMem().getOwnGoal();
-
-
-		if(flag != null) {
-
-			Pos flagCoord = getMem().getFlagPos(flag.getFlagName());
-			Pos toFlag = mh.getPos(flag.getDistance(), getMem().getDirectionOfSpeed() + flag.getDirection());
-			Pos self = mh.vSub(flagCoord, toFlag);
-
-			return(self);
-
-		}
-		else if(goal != null) {
-
-			Pos goalCoord = getMem().getOwnGoalPos();
-			Pos toGoal = mh.getPos(goal.getDistance(), getMem().getDirectionOfSpeed() + goal.getDirection());
-			Pos self = mh.vSub(goalCoord, toGoal);
-
-			return(self);
-		}
-		else {
-			flag = getMem().getClosestBoundary();
-
-			Pos flagCoord = getMem().getFlagPos(flag.getFlagName());
-			Pos toFlag = mh.getPos(flag.getDistance(), getMem().getDirectionOfSpeed() + flag.getDirection());
-			Pos self = mh.vSub(flagCoord, toFlag);
-
-			return(self);
-
-		}
-
-	}
-
-	/**
-	 * Turns goalie toward ball
-	 * @throws InterruptedException 
-	 * 
-	 * @post The goalie will turn in the direction of the ball
-	 */
-	public void followBall() throws InterruptedException {
-
-
-		try {
-			if(!getMem().isObjVisible("ball") && !ballTurn) {
-				getRoboClient().turn(35);
-				ballTurn = true;
-			}
-			else if(!getMem().isObjVisible("ball") && ballTurn) {
-				getRoboClient().turn(-90);
-				ballTurn = false;
-			}
-
-			if(getMem().isObjVisible("ball")) {
-				ObjBall ball = getMem().getBall();
-
-				if((ball.getDirection() > 5.0) || (ball.getDirection() < -5.0)) {
-					getRoboClient().turn(ball.getDirection() * (1 + (5 * getMem().getAmountOfSpeed())));
-				}
-
-				if(ballInGoalzone(ball)) {
-
-					defendGoal(ball);
-				}
-			}
-
-		} catch (UnknownHostException e) {
-			System.out.println("Error in Goalie.followBall()");
-			e.printStackTrace();
-		}
-=======
->>>>>>> 1bbb2ad4d3dae0e42c891a5cc61d5c0d6e86ef60
-	}
-	
 	/**
 	 * Gets absolute position of Goalie on field
 	 * @post Pos with x and y coordinates of Goalie on field
@@ -193,27 +112,30 @@ public class Goalie extends Player {
 	 * @post The goalie will turn in the direction of the ball
 	 */
 	public void followBall() {
-		
-		
+
+
 		try {
 			if(!getMem().isObjVisible("ball")) {
 				getRoboClient().turn(45);
 			}
 			if(getMem().isObjVisible("ball")) {
 				ObjBall ball = getMem().getBall();
-				
+
 				if((ball.getDirection() > 5.0) || (ball.getDirection() < -5.0)) {
 					getRoboClient().turn(ball.getDirection() * (1 + (5 * getMem().getAmountOfSpeed())));
 				}
-					
+
 				if(ballInGoalzone(ball)) {
-					
+
 					defendGoal(ball);
 				}
 			}
-			
+
 		} catch (UnknownHostException e) {
 			System.out.println("Error in Goalie.followBall()");
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -251,55 +173,6 @@ public class Goalie extends Player {
 	}
 	
 	/**
-	 * Causes the goalie to act to intercept the ball as it approaches the goal.
-	 * @param ObjBall representing the ball in play.
-	 * @throws UnknownHostException 
-	 * @pre The ball has entered the goal zone.
-	 * @post The ball has been caught by the goalie, or the goalie has missed the ball.
-	 */
-	public void defendGoal(ObjBall ball) throws UnknownHostException {				
-
-		//Move to catchable range of ball
-		getAction().gotoPoint(mh.getNextBallPoint(ball));
-		
-		//If ball is in catchable area, catch it
-		if (catchable()){
-			catchball(getMem().getBall().getDirection());
-		}		
-	}
-
-	/**
-	 * Returns the closest player to the goalie as an ObjPlayer object from the memory
-	 * of the current worldstate.
-	 * @pre Players are in sight of the goalie.
-	 * @post The closest player to the goalie has been determined.
-	 * @return ObjPlayer
-	 */
-	public ObjPlayer closestPlayer() {
-		ObjPlayer closestPlayer = new ObjPlayer();
-		double distance = 0;
-		
-		//Loop through arraylist of ObjPlayers
-		for (int i = 0; i < getMem().getPlayers().size(); ++i) {
-			
-			if (!getMem().getPlayers().isEmpty()) {  
-				if (distance == 0) {
-					distance = getMem().getPlayers().get(i).getDistance();
-				}
-				else {
-					
-					//Test if this player is closer than the previous one
-					if (distance > getMem().getPlayers().get(i).getDistance()) {
-						distance = getMem().getPlayers().get(i).getDistance();
-						closestPlayer = getMem().getPlayers().get(i);
-					}
-				}
-			}
-		}		
-		return closestPlayer;
-	}
-	
-	/**
 	 * Returns true or false depending on whether the ball is within the catchable range
 	 * of the goalie.
 	 * @pre The ball is visible to the goalie
@@ -326,34 +199,7 @@ public class Goalie extends Player {
 		return catchable;
 	}
 	
-	public boolean ballTurn = false;
-	public MathHelp mh = new MathHelp();
 
-
-	/**
-	 * A method to determine whether the ball is in the penalty box
-	 * 
-	 * @param ball the ObjBall to follow
-	 * @pre this must be called with an ObjBall
-	 * @post true if ball is in penalty box, false if it's not
-	 * @return boolean
-	 */
-	public boolean ballInGoalzone(ObjBall ball) {
-
-		if(ball == null)
-			return false;
-
-
-		Pos ballPos = mh.getPos(ball.getDistance(), getMem().getDirectionOfSpeed());
-		ballPos = mh.vAdd(getPosition(), ballPos);
-
-		if((ballPos.x <= -36) && ((-20.16 <= ballPos.y) && (ballPos.y <= 20.16)))
-			return true;
-		else
-			return false;
-
-
-	}
 
 	/**
 	 * Causes the goalie to act to intercept the ball as it approaches the goal.
@@ -407,34 +253,6 @@ public class Goalie extends Player {
 		}		
 		return closestPlayer;
 	}
-
-	/**
-	 * Returns true or false depending on whether the ball is within the catchable range
-	 * of the goalie.
-	 * @pre The ball is visible to the goalie
-	 * @post The ball is determined to catchable or not.
-	 * @return boolean True if catchable, false if not.
-	 */
-	public boolean catchable() {
-
-		boolean catchable = false;
-
-		//Test for visibility
-		if (getMem().isObjVisible("ball")) {
-
-			//Test for moment range
-			if (getMem().getBall().getDirection()> -180 && getMem().getBall().getDirection() < 180) {
-
-				//Test for catchable distance
-				if (getMem().getBall().getDistance() < 2.0) {
-					catchable = true;
-				}
-			}
-		}
-
-		return catchable;
-	}
-	
 
 	public void kickBallOutOfBounds() {
 		try {
